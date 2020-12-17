22acc684ac791cc6fa5ff4e2f2eca4164fb09c11
A-SITE <info@a-site.by>
2020-12-17 18:44:29 +0300

    111112

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ error_reporting(E_ALL);
 define('ROOT',dirname(__FILE__));
 session_start();
 
-//8111!!!!!!!!111
+//8111!!!!!!!!111222222
 require_once ROOT . '/autoload.php';
 require_once ROOT . '/functions.php';
 
