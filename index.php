7153374a67ddf38964504fc1cfabefcb0765d571
A-SITE <info@a-site.by>
2020-12-17 16:10:31 +0300

    111

--- index.php
+++ index.php
@@ -7,7 +7,6 @@ error_reporting(E_ALL);
 define('ROOT',dirname(__FILE__));
 session_start();
 
-var_dump(floor( (0.1 + 0.7) * 10 ));
 //8
 require_once ROOT . '/autoload.php';
 require_once ROOT . '/functions.php';
